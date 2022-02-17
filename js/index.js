70a9c633cbe79ea37fce7adb41eda0408de1d133
Adrian Reyes <ardprunie91@hotmail.com>
2022-02-16 21:40:47 -0600

    No se hizo cambios para el menu de js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /*declaramos dos objetos y obtendermos el primer elemento del documento con la clase del css*/
-const navToggle = document.querySelector(".navbar-toggle");
-const navMenu = document.querySelector(".navbar-menu");
+const navToggle = document.querySelector(".navbar-toggler");
+const navMenu = document.querySelector(".navbar-nav");
 
 /*le agregamos un escuchador y le indicamos que nos oculte el menu de inicio en la hamburguesa*/
 navToggle.addEventListener("click", () => {
