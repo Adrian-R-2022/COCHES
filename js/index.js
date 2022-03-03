a71482b81f926efe8ab6f480c5318e5568ff2f22
Adrian Reyes <ardprunie91@hotmail.com>
2022-03-02 18:09:25 -0600

    Quitando un archivo js innecesario

--- js/index.js
+++
@@ -1,8 +0,0 @@
-/*declaramos dos objetos y obtendermos el primer elemento del documento con la clase del css*/
-const navToggle = document.querySelector(".navbar-toggler");
-const navMenu = document.querySelector(".navbar-nav");
-
-/*le agregamos un escuchador y le indicamos que nos oculte el menu de inicio en la hamburguesa*/
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("navbar-menu_visible");
-});
